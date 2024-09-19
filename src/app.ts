255ee1322b5c0e26dec5fe80265319af9e4b4eb2
kbventures <kmb_x@hotmail.com>
2024-09-19 18:22:06 -0500

    Enhanced inital setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from 'express';
-const app = express()
+const app = express();
+import cors from 'cors';
 
-const port = 3000;
+console.log('Testing port env', process.env.PORT);
+const port = process.env.PORT || 4000;
 
-console.log("Testing port env", process.env.PORT)
+app.use(cors())
 
-app.get('/', (req: Request,res:Response)=>{
-    res.send('Hello World!')
-})
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+try {
+  app.listen(port, () => {
+    console.log(`Server running on port: `, port);
+  });
+} catch(err) {
+    if(err instanceof Error){
+        console.log(err.message)
+        console.log(err.stack)
+        console.log(err.name)
+    } else {
+        console.log("Unexpected Error: ", err)
+    }
+}

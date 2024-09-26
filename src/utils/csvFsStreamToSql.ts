379e56988a7f969f82673279e28cde0a5fa30b74
kbventures <kmb_x@hotmail.com>
2024-09-26 14:41:34 -0500

    Streamer CSV to SQL

--- src/utils/csvFsStreamToSql.ts
+++ src/utils/csvFsStreamToSql.ts
@@ -2,9 +2,13 @@ import path from 'path';
 import fs from 'fs';
 import * as fastcsv from 'fast-csv';
 import myTransformStream  from './transformStream.js';
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { OHLCVT, ChunkCount } from '../types/index';
 import { updateGlobalVariables } from './updateGlobalVariables.js';
+import { Decimal } from 'decimal.js';
+
+
+const Prisma = new PrismaClient();
 
 // // Obtain pair name and interval from execution command line
 const newCurrencyPair = process.env.NEWPAIR || '';
@@ -24,7 +28,6 @@ const csvLocation = path.join(
   fileName
 );
 
-
 fs.createReadStream(csvLocation)
   .pipe(fastcsv.parse({ headers: false }))
   .pipe(myTransformStream((data)=>{
@@ -32,6 +35,41 @@ fs.createReadStream(csvLocation)
     currentChunks = updatedState.chunks; 
     currentChunkCount = updatedState.count;
     console.log(currentChunks,currentChunkCount)
+
+    //****************
+    if(currentChunkCount === 2){
+      currentChunks.forEach(async e=>{
+        console.log(e)
+        await Prisma.oHLCVT1.create({
+          data:{
+            timestamp: e.timestamp,
+            open: new Decimal(e.open),
+            high: new Decimal(e.high),
+            low: new Decimal(e.low),
+            close: new Decimal(e.close),
+            volume: new Decimal(e.volume),
+            transactionCount: e.transactionCount,
+            currencyPairId:"XBTCAD"
+          }
+        })
+      })
+      currentChunkCount=0;
+      currentChunks=[];
+    } 
+
+    // //***************
+    // currentChunks.forEach(e=>{
+    //   console.log(e)
+    // })
+    // {
+    //   timestamp: 2015-06-30T04:22:00.000Z,
+    //   open: 319.99,
+    //   high: 319.99,
+    //   low: 319.99,
+    //   close: 319.99,
+    //   volume: 0.5,
+    //   transactionCount: 1
+    // }
   }))
   .on('error', (error) => console.error(error))
   .on('end', () => {
@@ -39,33 +77,3 @@ fs.createReadStream(csvLocation)
     console.log("currentCunks", currentChunks)
   });
 
-// prisma.$disconnect(); // Disconnect Prisma client
-
-// Todo Prisma Disconect Location?
-// Add entries to Postgresql and make sure it works
-// Add in chunks
-// Error handling
-// Automated Testing
-
-// // 1 000 000 OHLCVT 1 Minute candles
-// // Unix Time Stamp, O, H, L, C, V, T
-// // 1435548420,315.0,315.0,315.0,315.0,0.25,1
-// // 1435612800,317.5,317.5,317.5,317.5,0.25,1
-// // 1435638120,319.99,319.99,319.99,319.99,0.5,1
-// // 1436433600,350.0,350.0,350.0,350.0,1,1
-// // 1436433960,350.0,350.0,350.0,350.0,0.0436,1
-// // 1436454240,365.0,365.0,365.0,365.0,0.2,1
-// // 1436458380,363.0,363.0,350.2,350.2,2.59710833,2
-// // 1436503320,350.0,350.0,350.0,350.0,0.4317,1
-// // 1436503380,350.0,350.0,350.0,350.0,0.0574,1
-// // 1436503500,350.0,350.0,350.0,350.0,0.0575,1
-// // 1436503860,350.0,350.0,350.0,350.0,0.1006,1
-// // 1436504100,350.0,350.0,350.0,350.0,0.0719,1
-// // 1436504160,350.0,350.0,350.0,350.0,0.0576,1
-// // 1436504460,350.0,350.0,350.0,350.0,0.0432,1
-// // 1436509080,350.0,350.0,350.0,350.0,0.2752,1
-// // 1436509560,350.0,350.0,350.0,350.0,0.1449,1
-// // 1436518260,350.2,350.2,350.2,350.2,0.1281,1
-// // 1436518500,350.2,350.2,350.2,350.2,0.0854,1
-// // 1436518680,350.2,350.2,350.2,350.2,0.18939167,1
-// // 1436671620,375.0,375.0,375.0,375.0,0.0399,1

294c174ffe35653a32e1f449cb8d707a20b77387
kbventures <kmb_x@hotmail.com>
2024-10-07 12:49:11 -0500

    CSV streamer to sql modularized

--- backend/src/utils/transformStream.ts
+++ backend/src/utils/transformStream.ts
@@ -1,7 +1,42 @@
+// import { Transform } from 'stream';
+// import { OHLCVT } from '../types';
+
+// function myTransformStream(updateGlobalVariables: (data: OHLCVT) => void) {
+//   const newTransform = new Transform({
+//     objectMode: true,
+//     async transform(row, encoding, callback) {
+//       const data = {
+//         // Assuming the CSV columns correspond to these properties
+//         timestamp: new Date(row[0] * 1000),
+//         open: Number(row[1]),
+//         high: Number(row[2]),
+//         low: Number(row[3]),
+//         close: Number(row[4]),
+//         volume: Number(row[5]),
+//         transactionCount: Number(row[6]),
+//       };
+//       // currentChunks.push(data);
+//       // console.log(data)
+//       // console.log(currentChunks)
+//       // currentChunkCount.count++;
+//       updateGlobalVariables(data);
+//       callback(null, data);
+//     },
+//   });
+//   return newTransform;
+// }
+
+// export default myTransformStream;
+
+// // First you need to transform the data and add
+// // its reference to an object
+
 import { Transform } from 'stream';
 import { OHLCVT } from '../types';
 
-function myTransformStream(updateGlobalVariables: (data: OHLCVT) => void) {
+export function myTransformStream(
+  updateGlobalVariables: (data: OHLCVT) => void
+) {
   const newTransform = new Transform({
     objectMode: true,
     async transform(row, encoding, callback) {
@@ -15,18 +50,9 @@ function myTransformStream(updateGlobalVariables: (data: OHLCVT) => void) {
         volume: Number(row[5]),
         transactionCount: Number(row[6]),
       };
-      // currentChunks.push(data);
-      // console.log(data)
-      // console.log(currentChunks)
-      // currentChunkCount.count++;
-      updateGlobalVariables(data);
+      await updateGlobalVariables(data);
       callback(null, data);
     },
   });
   return newTransform;
 }
-
-export default myTransformStream;
-
-// First you need to transform the data and add
-// its reference to an object

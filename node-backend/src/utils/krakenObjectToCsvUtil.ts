d80709d08c8d41ed69ae6db2d6052b7cfc3e246a
kbventures <kmb_x@hotmail.com>
2024-10-10 16:57:27 -0500

    Kraken Obbjet to Csv Util

--- node-backend/src/utils/krakenObjectToCsvUtil.ts
+++ node-backend/src/utils/krakenObjectToCsvUtil.ts
@@ -3,7 +3,6 @@ import { getKrakenOHLCVTData } from './krakenApiFetchFunction.js';
 import { OHLCVT } from '../types/index.js';
 
 const [, , pair, interval, since]: string[] = process.argv;
-console.log('sanity check');
 
 interface KrakenOHLVCVTResponse {
   result: Record<string, OHLCVT[]>;
@@ -19,18 +18,23 @@ if (ohlcvtUpdate && ohlcvtUpdate.result && ohlcvtUpdate.result.XBTCAD) {
   throw new Error('Failed to fetch data or no data returned');
 }
 
-// for(t of ohlcvtUpdate.result.XXBTZCAD){
-//     console.log(t)
-// }
-// const csvContent = ohlcvtUpdate.result.XXBTZCAD.map(row=>row.join(',')).join('\n');
+const csvContent = ohlcvtUpdate.result.XXBTZCAD.map(
+  (row) =>
+    `${row.timestamp},${row.open},${row.high},${row.low},${row.close},${row.volume},${row.transactionCount}`
+).join('\n');
 
-// try{
-// fs.writeFileSync(`${pair}_${interval}`, csvContent =>{
-// });
-// } catch(err:unknown){
-//     if(err instanceof Error){
-//         console.error("Error writing to file: ", err, err.message, err.name, err.stack)
-//     } else {
-//         throw new Error("Unknown error while writing to file: ")
-//     }
-// }
+try {
+  fs.writeFileSync(`${pair}_${interval}.csv`, csvContent);
+} catch (err: unknown) {
+  if (err instanceof Error) {
+    console.error(
+      'Error writing to file: ',
+      err,
+      err.message,
+      err.name,
+      err.stack
+    );
+  } else {
+    throw new Error('Unknown error while writing to file: ');
+  }
+}
